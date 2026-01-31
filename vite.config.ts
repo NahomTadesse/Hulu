bad8b3e769c14fbac8c4fd4e1e15c8a31beb0c16
Nahom <nahitad12@gmail.com>
2026-01-31 12:04:12 +0300

    inital hulu

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+
+    optimizeDeps: {
+    include: ['react-is']
+  },
+  
   plugins: [
     tailwindcss(),
     react()
